9e8646100b9373b72010e0ad1e95456a9346c261
vagrant <vagrant@ubuntu-oneiric.(none)>
2013-05-27 13:07:21 +0100

    Added manifest file

--- setup.py
+++ setup.py
@@ -2,7 +2,8 @@ from setuptools import find_packages
 from isc_ops.setup_tools import setup, current_version
 
 setup(name='onionconfig',
-    packages=find_packages(),  
+    packages=find_packages(), 
+    include_package_data=True, 
     description = 'Inheritable configuration',
     url = 'http://github.com/infoscout/onionconfig',
     version = '1.0',    
