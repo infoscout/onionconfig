2e3c8ef0e6b4fef23a8ec415fefb64cbbfe41c40
Lucas Connors <lucas.revolutiontech@gmail.com>
2017-02-02 13:54:37 -0800

    CA-5541 PEP8: Improve spacing

--- setup.py
+++ setup.py
@@ -1,16 +1,18 @@
 from setuptools import find_packages, setup
 
-with open('VERSION','r') as f:
+
+with open('VERSION', 'r') as f:
     version = f.read().strip()
-    
-setup(name='onionconfig',
-    packages=find_packages(), 
-    include_package_data=True, 
-    description = 'Inheritable configuration',
-    url = 'http://github.com/infoscout/onionconfig',
-    version = version,    
+
+
+setup(
+    name='onionconfig',
+    packages=find_packages(),
+    include_package_data=True,
+    description='Inheritable configuration',
+    url='http://github.com/infoscout/onionconfig',
+    version=version,
     install_requires=[
         'django>=1.4',
     ]
 )
-
